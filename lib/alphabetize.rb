8517c5f005f0a947ceb58843b1a9be7cdb219408
Matt Maher <mattmaher0@gmail.com>
2018-05-23 18:31:40 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,5 @@
 espranto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  arr.sort_by! {|word| word[0].index("a") <=> word[0].index("z")}
+  arr.sort_by! {|word| word[0].index("z") <=> word[0].index("a")}
 end
